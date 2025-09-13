5781317fa0ae18a2918e59b1c5c122af4aa8e2dc
William dos Santos Rodrigues <williamdossantosrodrigues94@gmail.com>
2025-09-13 18:10:23 -0300

    ajustes no login

--- DF_Vistoria/src/Vistoria/Main.java
+++ DF_Vistoria/src/Vistoria/Main.java
@@ -8,17 +8,18 @@ import Vistoria.View.Login;
 
 public class Main {
 
-    public static void main(String[] args) {
-        try {
-            // Define o tema FlatLaf (pode trocar por FlatDarkLaf, FlatIntelliJLaf, FlatDarculaLaf)
-            UIManager.setLookAndFeel(new FlatIntelliJLaf());
-        } catch (UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        }
+	public static void main(String[] args) {
+		try {
+			// Define o tema FlatLaf (pode trocar por FlatDarkLaf, FlatIntelliJLaf,
+			// FlatDarculaLaf)
+			UIManager.setLookAndFeel(new FlatIntelliJLaf());
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
 
-        // Inicia a aplicação com a tela de login
-        java.awt.EventQueue.invokeLater(() -> {
-            new Login().setVisible(true);
-        });
-    }
+		// Inicia a aplicação com a tela de login
+		java.awt.EventQueue.invokeLater(() -> {
+			new Login().setVisible(true);
+		});
+	}
 }
--- DF_Vistoria/src/Vistoria/View/DashboardVistoria.java
+++ DF_Vistoria/src/Vistoria/View/DashboardVistoria.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 public class DashboardVistoria extends JFrame {
 
@@ -36,6 +38,24 @@ public class DashboardVistoria extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(127)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(287, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(159, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
 
 	}
 
